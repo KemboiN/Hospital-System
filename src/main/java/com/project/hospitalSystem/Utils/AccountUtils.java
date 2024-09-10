556101ddae294029cbd03f17a20964adf8d07a88
Kemboi Nehemiah <nehemiahkimutai32@gmail.com>
2024-09-10 08:58:14 +0300

    Utils update

--- src/main/java/com/project/hospitalSystem/Utils/AccountUtils.java
+++ src/main/java/com/project/hospitalSystem/Utils/AccountUtils.java
@@ -5,5 +5,5 @@ public class AccountUtils
     public static  final String Account_Creation_Response_Code="001";
     public static  final String Account_Creation_Response_Message="Congratulations, you have registered with BEST Hospital";
     public static  final String Account_Exists_Response_Code="002";
-    public static  final String Account_Exists_Response_Message="You already have an Account with BEST Hospital";
+    public static  final String Account_Exists_Response_Message="You already have an Account with BEST Hospital Try to register";
 }
